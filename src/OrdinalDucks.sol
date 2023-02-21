2fa185433e20f5f3c4d6a0863366a0c8dfd4a872
Zodomo <zodomo@proton.me>
2023-02-20 19:36:23 -0600

    init

---
+++ src/OrdinalDucks.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+/// @title An ERC721 contract that mints placeholder NFTs that a recipient can burn alongside providing a Bitcoin address for the team to manually send their ordinal to.
+/// @author Zodomo
+/// @notice This contract is designed with the idea in mind that the team would be manually managing sending ordinals to their recipients as to avoid using complicated technologies like the Emblem Vaults.
+contract OrdinalDucks {
+    
+}
